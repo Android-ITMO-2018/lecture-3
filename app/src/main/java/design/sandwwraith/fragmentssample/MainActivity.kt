45a0bd9ba8f70e0783cc855190b439850f3d40c2
Leonid Startsev <sandwwraith@gmail.com>
2018-10-09 23:36:48 +0300

    Initial state w/o fragments

--- app/src/main/java/design/sandwwraith/fragmentssample/MainActivity.kt
+++ app/src/main/java/design/sandwwraith/fragmentssample/MainActivity.kt
@@ -2,11 +2,17 @@ package design.sandwwraith.fragmentssample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.users_list.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        users_list.setupForUsers(this, 20) {
+            Toast.makeText(this, "Clicked $it item", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/design/sandwwraith/fragmentssample/Users.kt
+++ app/src/main/java/design/sandwwraith/fragmentssample/Users.kt
@@ -1,15 +1,38 @@
 package design.sandwwraith.fragmentssample
 
+import android.content.Context
 import android.os.Parcelable
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import kotlinx.android.parcel.Parcelize
 import kotlinx.android.synthetic.main.item_user.view.*
+import java.util.*
 
 @Parcelize
 data class User(val firstName: String, val lastName: String, val age: Int) : Parcelable
+
+fun generateUsers(size: Int): List<User> {
+    val res = mutableListOf<User>()
+    val rand = Random()
+    repeat(size) {
+        res.add(when(rand.nextInt(4)) {
+            0 -> User("John", "Doe", rand.nextInt(20) + 10)
+            1 -> User("John", "Cena", rand.nextInt(30) + 5)
+            2 -> User("Joseph", "Joestar", rand.nextInt(10) + 50)
+            else -> User("Unknown", "Hero", rand.nextInt(100))
+        })
+    }
+    return res
+}
+
+fun RecyclerView.setupForUsers(ctx: Context, genSize: Int, onItemClicked: (Int) -> Unit = {}) {
+    layoutManager = LinearLayoutManager(ctx)
+    adapter = UsersRecycler(generateUsers(genSize), onItemClicked)
+    setHasFixedSize(true)
+}
 
 class UserViewHolder(val frame: FrameLayout) : RecyclerView.ViewHolder(frame) {
     val textView = frame.item_text!!
